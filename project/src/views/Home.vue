4a915dcc09bc2bc76ce5570ca7aaadbf93067dff
Alex <aelmore@merkleinc.com>
2022-03-08 07:55:13 -0600

    Make fetch request to json db for project inside of the Home Component

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="home">
-    home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        {{ project.title }}
+      </div>
+    </div>
+    <div v-else>Loading Projects...</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  components: {},
+  // Mounted hook for sending out Get Fetch request to json DB as soon as component is mounted to the DOM.
+  mounted() {
+    // Setup url and GET fetch request going to our json db file
+    const url = `http://localhost:3000/projects`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        // Init our projects property with the returned data from our Fetch request
+        this.projects = data;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  },
 };
 </script>
