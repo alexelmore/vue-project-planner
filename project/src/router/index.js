b23c1bc4b9a1f3c6d1337695841e25d78710f219
Alex <aelmore@merkleinc.com>
2022-03-08 11:00:19 -0600

    Setup for adding a new project to our projects data from within the AddProject Component

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
   },
   {
     path: "/add",
-    name: "Add",
+    name: "AddProject",
     component: AddProject,
   },
   {
--- project/src/views/AddProject.vue
+++ project/src/views/AddProject.vue
@@ -1,11 +1,67 @@
 <template>
-  <div>Add Project</div>
+  <form @submit.prevent="handleSubmit">
+    <label>Project Title </label>
+    <input required type="text" v-model="title" />
+    <label>Project Details </label
+    ><textarea required v-model="details"></textarea>
+    <button>Add Project</button>
+  </form>
 </template>
 
 <script>
 export default {
   name: "AddProject",
+  data() {
+    return {
+      title: "",
+      details: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.title, this.details);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+</style>
