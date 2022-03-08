e4a7f516218b27a4aa199cba40c16b2da8edd039
Alex <aelmore@merkleinc.com>
2022-03-08 08:19:30 -0600

    Setup for SingleProject Component

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        {{ project.title }}
+        <SingleProject :project="project" />
       </div>
     </div>
     <div v-else>Loading Projects...</div>
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
 export default {
   name: "Home",
   data() {
@@ -17,7 +18,7 @@ export default {
       projects: [],
     };
   },
-  components: {},
+  components: { SingleProject },
   // Mounted hook for sending out Get Fetch request to json DB as soon as component is mounted to the DOM.
   mounted() {
     // Setup url and GET fetch request going to our json db file
